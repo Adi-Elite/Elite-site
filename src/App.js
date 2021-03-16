7be773cb1b16d0ffe2b349aa22a6789b53de2d61
elitemantra <davidelitemantra@gmail.com>
2021-03-16 16:19:22 +0530

    update

--- src/App.js
+++ src/App.js
@@ -3,16 +3,22 @@ import {NavBar} from './Components/Navbar.js';
 import {About} from './Components/About.js';
 import {WebDev} from './Components/Services/WebDev.js';
 import {Home} from './Components/Home.js';
-import {BrowserRouter as Router,Route,Switch} from 'react-router-dom';
 
-function App() {
+
+import {BrowserRouter as Router,Route,Switch} from 'react-router-dom';
+import {ContactUs} from './Components/ContactUs.js';
+
+function App() {  
   return (
     <div className="App">
       <NavBar/>
-      <Router>
+      
+    
+    <Router>
         <Route exact path="/" component={Home}/>
         <Route exact path="/about" component={About}/>
         <Route exact path="/services/webdev" component={WebDev}/>
+        <Route exact path="/contact" component={ContactUs}/>
       </Router>
     </div>
   );
---
+++ src/Components/ContactUs.js
@@ -0,0 +1,74 @@
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import logo from "../Assets/logo.png";
+import  './ContactUs.css';
+
+export  class ContactUs extends Component {
+    render() {
+        return (
+          <div>
+            <div>
+              <div>
+                <div className="container contact-form" style={{textAlign:"center"}}>
+                  <div className="contact-image">
+                  <img src={logo}/>
+                  </div>
+                  <form method="post">
+                    <h3>Contact Us</h3>
+                    <div className="row">
+                      <div className="col-md-6">
+                        <div className="form-group">
+                          <input
+                            type="text"
+                            name="txtName"
+                            className="form-control"
+                            placeholder="Your Name *"
+                            value=""
+                          />
+                        </div>
+                        <div className="form-group">
+                          <input
+                            type="text"
+                            name="txtEmail"
+                            className="form-control"
+                            placeholder="Your Email *"
+                            value=""
+                          />
+                        </div>
+                        <div className="form-group">
+                          <input
+                            type="text"
+                            name="txtPhone"
+                            className="form-control"
+                            placeholder="Your Phone Number *"
+                            value=""
+                          />
+                        </div>
+                        <div className="form-group">
+                          <input
+                            type="submit"
+                            name="btnSubmit"
+                            className="btnContact"
+                            value="Send Message"
+                          />
+                        </div>
+                      </div>
+                      <div className="col-md-6">
+                        <div className="form-group">
+                          <textarea
+                            name="txtMsg"
+                            className="form-control"
+                            placeholder="Your Message *"
+                            style={{width: "100%",height: "150px"}}
+                          ></textarea>
+                        </div>
+                      </div>
+                    </div>
+                  </form>
+                </div>
+              </div>
+            </div>
+          </div>
+        );
+    }
+}
